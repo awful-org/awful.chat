debd6839425c0dc4b23dbefa910ac34033ebcbfd
FlavioZanoni <flaviozg888@gmail.com>
2026-03-17 19:59:12 -0300

    Registering worker

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -3,3 +3,14 @@
 /// <reference types="vite-plugin-pwa/svelte" />
 /// <reference types="vite-plugin-pwa/info" />
 /// <reference lib="webworker" />
+
+declare module 'virtual:pwa-register' {
+  export function registerSW(options?: {
+    onNeedRefresh?: () => void
+    onOfflineReady?: () => void
+    onRegistered?: (registration: ServiceWorkerRegistration | undefined) => void
+    onRegisteredSW?: (swUrl: string, registration: ServiceWorkerRegistration | undefined) => void
+    register?: (val: boolean) => void
+    immediate?: boolean
+  }): () => void
+}
