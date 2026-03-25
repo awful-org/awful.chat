b0751ed0ffe3a39e7d7da61f67089ff40d6df2ad
FlavioZanoni <flaviozg888@gmail.com>
2026-03-24 23:01:24 -0300

    Increasing pwa max cache size

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,12 +11,15 @@ export default defineConfig({
     tailwindcss(),
     svelte(),
     nodePolyfills(),
-    VitePWA({
-      registerType: "autoUpdate",
-      strategies: "injectManifest",
-      srcDir: "src",
-      filename: "sw.ts",
-      includeAssets: ["favicon.ico", "apple-touch-icon-180x180.png"],
+VitePWA({
+			registerType: "autoUpdate",
+			strategies: "injectManifest",
+			srcDir: "src",
+			filename: "sw.ts",
+			injectManifest: {
+				maximumFileSizeToCacheInBytes: 3 * 1024 * 1024, // 3 MB
+			},
+			includeAssets: ["favicon.ico", "apple-touch-icon-180x180.png"],
       manifest: {
         name: "Awful.chat",
         short_name: "Awful.chat",
